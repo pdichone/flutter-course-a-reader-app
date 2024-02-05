b423b2f2f43f82300723978b6da7abc702dbde87
Paulo <pdichone@gmail.com>
2024-02-05 12:35:48 -0800

    Add book image and titles to DetailsScreen

--- lib/pages/books_details.dart
+++ lib/pages/books_details.dart
@@ -15,10 +15,52 @@ class _BookDetailsScreenState extends State<BookDetailsScreen> {
     final args =
         ModalRoute.of(context)?.settings.arguments as BookDetailsArguments;
     final Book book = args.itemBook;
+    final theme = Theme.of(context).textTheme;
 
     return Scaffold(
       appBar: AppBar(
         title: Text(book.title),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              if (book.imageLinks.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.network(
+                    book.imageLinks['thumbnail'] ?? '',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    book.title,
+                    style: theme.headlineSmall,
+                  ),
+                  Text(
+                    book.authors.join(', '),
+                    style: theme.labelLarge,
+                  ),
+                  Text(
+                    'Published: ${book.publishedDate}',
+                    style: theme.bodySmall,
+                  ),
+                  Text(
+                    'Page count: ${book.pageCount}',
+                    style: theme.bodySmall,
+                  ),
+                  Text(
+                    'Language: ${book.language}',
+                    style: theme.bodySmall,
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
